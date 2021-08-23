9c967d183b11a25feb74a41870616172f5e4f888
Nur Syahfitriani Podang <scha.fitriani@gmail.com>
2021-08-23 21:15:15 +0800

    NUR SYAHFITRIANI PODANG
    sunbear facts

--- js/main.js
+++ js/main.js
@@ -10,3 +10,57 @@ closeBars.addEventListener("click", function(){
     mobileMenu.style.display = "none"
     document.querySelector("#mobile_menu").classList.add("hide")
 })
+
+// newsletter
+let newsLetter = document.querySelector(".newsletter_popup")
+let sideButton = document.querySelector("#sideBtn")
+let closeSide = document.querySelector("#close_side")
+
+sideButton.addEventListener ("click", function(){
+    newsLetter.style.display = "block"
+    document.querySelector("#subscription").classList.add("show")
+})
+closeSide.addEventListener("click", function(){
+    newsLetter.style.display = "none"
+    document.querySelector("#subscription").classList.add("hide")
+})
+
+
+// gallery 
+
+// let images = [
+//     "img\sun bear on tree.jpg",
+//     "img\sun bear termenung.jpg",
+//     "img\tyre swing sun bear.jpg",
+//     "img\volunteer bear 1.jpg"
+//     ]
+
+
+// let next = document.querySelector("#nextBtn")
+// let previous = document.querySelector("#previousBtn")
+// let sliderImage = document.querySelector("#slider_image")
+// let i = 0 //current image index
+
+// next.addEventListener ("click", function(){
+//     sliderImage.innerHTML = (images[i]);
+//     if(i === 3) {
+//         i = 0;
+//     }
+//     else {
+//         i++;
+//     }
+// })
+
+// previous.addEventListener("click", function(){
+//     sliderImage.innerHTML = (images[i]);
+//     if (i === 0) {
+//         i = 3;
+//     }    
+//     else {
+//         i--;    
+//     }
+// })
+
+
+
+
