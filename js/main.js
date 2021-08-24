783d97d12b6c64d0f211fc324abbfb8de7915e99
Nur Syahfitriani Podang <scha.fitriani@gmail.com>
2021-08-24 08:17:20 +0800

    js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,37 @@
-let mobileMenu = document.querySelector("#mobile_menu")
-let mobileBars = document.querySelector("#bars")
-let closeBars = document.querySelector("#close")
-
-mobileBars.addEventListener ("click", function(){
-    mobileMenu.style.display = "block"
-    document.querySelector("#mobile_menu").classList.add("show")
-})
-closeBars.addEventListener("click", function(){
-    mobileMenu.style.display = "none"
-    document.querySelector("#mobile_menu").classList.add("hide")
-})
+// mobile menu
+document.querySelector("button").addEventListener("click", function () {
+    document.querySelector("#mobile_menu").classList.add("show");
+  });
+  
+  document.querySelector("#close").addEventListener("click", function () {
+    document.querySelector("#mobile_menu").classList.remove("show");
+    // document.querySelector("#mobile_menu").classList.add("hide")
+  });
+  
+  // newsletter
+  let newsLetter = document.querySelector(".newsletter_popup");
+  let sideButton = document.querySelector("#sideBtn");
+  let closeSide = document.querySelector("#close_side");
+  
+  sideButton.addEventListener("click", function () {
+    newsLetter.style.display = "block";
+    document.querySelector("#subscription").classList.add("show");
+  });
+  closeSide.addEventListener("click", function () {
+    newsLetter.style.display = "none";
+    document.querySelector("#subscription").classList.remove("show");
+    // document.querySelector("#subscription").classList.add("hide")
+  });
+  
+  // gallery
+  
+  // let images = [
+  //     "img\sun bear on tree.jpg",
+  //     "img\sun bear termenung.jpg",
+  //     "img\tyre swing sun bear.jpg",
+  //     "img\volunteer bear 1.jpg"
+  
+  // let next = document.querySelector("#nextBtn")
+  // let previous = document.querySelector("#previousBtn")
+  // let sliderImage = document.querySelector("#slider_image")
+  // let i = 0 //current image index
